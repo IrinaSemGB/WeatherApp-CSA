c9c51df4b654631bfb1a9f34a1cb53f8ebc11bf4
IrinaSemGB <irina.semyachkina@mail.ru>
2020-06-15 09:49:31 +0300

    Initial Commit

---
+++ WeatherApp-CSA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp-CSA
+//
+//  Created by Ирина Семячкина on 15.06.2020.
+//  Copyright © 2020 Ирина Семячкина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
